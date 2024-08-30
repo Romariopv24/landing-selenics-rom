14684be21ba32bc7af3e42c876d1952302f01196
Romario Parra <rparra@devtop.io>
2024-08-29 21:58:19 -0400

    Video background fix the screen with mediaquerys lol

--- src/app/components/layoutSection/page.tsx
+++ src/app/components/layoutSection/page.tsx
@@ -5,49 +5,99 @@ import { Navbar } from '../navigation/navbar/page'
 
 export const LayoutSection = ({children}: {children: React.ReactNode}) => {
   return (
-    <Stack sx={{
-      display:'flex', 
-      alignItems:'center',
-      alignItem: "center",
-      flexDirection: "column",
-      justifyContent: "center",
-      height: {lg: "100vh" },
+    <Stack
+    sx={{
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      minHeight: {lg:'100vh', xs: '90vh'},
       position: 'relative',
       overflow: 'hidden',
       zIndex: 1,
-             }}>
-            <video 
-            width="100%"
-            height="100%"
-            style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                zIndex: -1,
-                objectFit: 'cover',
-                pointerEvents: 'none', 
-            }}
-           loop autoPlay muted>
-         <source src={rootImages.video.starsBg} type="video/mp4" />
-   
-     </video>
-           {children}
-    </Stack>
+    }}
+  >
+    <Box
+      sx={{
+        width: '100%',
+        padding: '0 20px', 
+        display: {lg: 'none'}
+      }}
+    >
+      <Stack
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          width:{xs:'510vw', sm: '410vw', md: '310vw'},
+        }}
+      >
+        <video
+          width="100%"
+          height="100%"
+          style={{
+            objectFit: 'cover',
+            pointerEvents: 'none',
+          }}
+          loop
+          autoPlay
+          muted
+        >
+          <source src={rootImages.video.starsBg} type="video/mp4" />
+        </video>
+      </Stack>
+
+      
+    </Box>
+    <Box display={{xs:'none', lg: 'flex'}}
+    >
+    <video 
+      width="100%"
+      height="100%"
+      style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          objectFit: 'cover',
+          pointerEvents: 'none', 
+          }}
+            loop autoPlay muted>
+          <source src={rootImages.video.starsBg} type="video/mp4" />
+          </video>
+          </Box>
+        {children}
+  </Stack>
   )
 }
 
 
-// const MainContainer = styled(Stack)(({theme}) => ({
-//     backgroundImage: rootImages.starsBg,
-//     display:'flex', 
-//     alignItems:'center',
-//     alignItem: "center",
-//     flexDirection: "column",
-//     justifyContent: "center",
-//     [theme.breakpoints.up('lg')]: {
-//         minHeight: '100vh',
-//       },
-//       [theme.breakpoints.down('lg')]: {
-//         minHeight: 'auto',
-//       },
-//   }))
+{/* <Stack sx={{
+  display:{xs:'none',lg:'flex'}, 
+  alignItems:'center',
+  alignItem: "center",
+  flexDirection: "column",
+  justifyContent: "center",
+  minHeight: '100vh',
+  position: 'relative',
+  overflow: 'hidden',
+  zIndex: 1,
+  backgroundColor:'red'
+ }}>
+    <video 
+      width="100%"
+      height="100%"
+      style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          objectFit: 'cover',
+          pointerEvents: 'none', 
+
+      }}
+    loop autoPlay muted>
+  <source src={rootImages.video.starsBg} type="video/mp4" />
+</video>
+</Stack> */}
