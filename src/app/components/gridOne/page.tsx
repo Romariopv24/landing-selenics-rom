194e3c19edb5dc9c22c8c99110a76d26bf43d8ec
Romario Parra <rparra@devtop.io>
2024-09-04 18:25:58 -0400

    mas ajustable, puede ser mejor

--- src/app/components/gridOne/page.tsx
+++ src/app/components/gridOne/page.tsx
@@ -13,10 +13,16 @@ export const FitComponent = () => {
       alignItems: 'center',
       flexDirection: 'column',
       minHeight: { lg: '100vh', xs: '90vh' },
-      background: `
-            radial-gradient(circle at 10% -50%, #5f1869, transparent 44%),
-            radial-gradient(circle at 95% -20%, #11649b, transparent 50%)
-                 `,
+      background: {
+        lg: `
+          radial-gradient(circle at 10% -50%, #5f1869, transparent 44%),
+          radial-gradient(circle at 95% -20%, #11649b, transparent 50%)
+        `,
+        xs: `
+          radial-gradient(circle at 10% -100%, #5f1869, transparent 44%),
+          radial-gradient(circle at 110% -30%, #11649b, transparent 70%)
+        `,
+      },
       padding: '2rem',
     }
     
