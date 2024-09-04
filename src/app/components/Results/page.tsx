9086659f38dc394b3decdb209e35f17b7dbf58be
Romario Parra <rparra@devtop.io>
2024-09-04 18:36:51 -0400

    asi se queda :)

--- src/app/components/Results/page.tsx
+++ src/app/components/Results/page.tsx
@@ -21,3 +21,5 @@ const mainContainer = {
              `,
   padding: '2rem',
 }
+
+
--- src/app/components/gridOne/page.tsx
+++ src/app/components/gridOne/page.tsx
@@ -14,17 +14,25 @@ export const FitComponent = () => {
       flexDirection: 'column',
       minHeight: { lg: '100vh', xs: '90vh' },
       background: {
+        xl: `
+            radial-gradient(circle at 5% -60%, #5f1869, transparent 44%),
+            radial-gradient(circle at 95% -18%, #11649b, transparent 50%),
+            radial-gradient(circle at 20% 90%, #34266e, transparent 50%)
+        `,
         lg: `
-          radial-gradient(circle at 10% -50%, #5f1869, transparent 44%),
-          radial-gradient(circle at 95% -20%, #11649b, transparent 50%)
+            radial-gradient(circle at 20% -60%, #5f1869, transparent 44%),
+            radial-gradient(circle at 95% -20%, #11649b, transparent 50%),
+            radial-gradient(circle at 20% 90%, #34266e, transparent 50%)
         `,
         xs: `
-          radial-gradient(circle at 10% -100%, #5f1869, transparent 44%),
-          radial-gradient(circle at 110% -30%, #11649b, transparent 70%)
+            radial-gradient(circle at 10% -100%, #5f1869, transparent 44%),
+            radial-gradient(circle at 110% -30%, #11649b, transparent 70%),
+            radial-gradient(circle at 20% 90%, #34266e, transparent 50%)
         `,
-      },
+        },
       padding: '2rem',
     }
     
 
   
+    // 34266e
