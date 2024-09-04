7d86f09d8dca40512eaaa563d29fce98bc404469
Romario Parra <rparra@devtop.io>
2024-09-04 18:15:00 -0400

    mejor

--- src/app/components/gridOne/page.tsx
+++ src/app/components/gridOne/page.tsx
@@ -14,8 +14,8 @@ export const FitComponent = () => {
       flexDirection: 'column',
       minHeight: { lg: '100vh', xs: '90vh' },
       background: `
-            radial-gradient(circle at 10% -50%, #5f1869, transparent 43%),
-            radial-gradient(circle at 95% -21%, #11649b, transparent 50%)
+            radial-gradient(circle at 10% -50%, #5f1869, transparent 44%),
+            radial-gradient(circle at 95% -20%, #11649b, transparent 50%)
                  `,
       padding: '2rem',
     }
