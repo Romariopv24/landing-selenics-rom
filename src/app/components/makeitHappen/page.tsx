b4931fe9ebdcb8bc42013f008fcc977c50518732
Romario Parra <rparra@devtop.io>
2024-09-07 16:19:18 -0400

    gradiente

--- src/app/components/makeitHappen/page.tsx
+++ src/app/components/makeitHappen/page.tsx
@@ -10,29 +10,28 @@ export const MakeItHappenContainer = () => {
   )
 }
 
-
 const mainContainer = {
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'column',
-    minHeight: { lg: '100vh', xs: '90vh' },
-    // background: {
-    //   xl: `
-    //       radial-gradient(circle at 5% -60%, #5f1869, transparent 44%),
-    //       radial-gradient(circle at 95% -18%, #11649b, transparent 50%),
-    //       radial-gradient(circle at 20% 90%, #34266e, transparent 50%)
-    //   `,
-    //   lg: `
-    //       radial-gradient(circle at 20% -60%, #5f1869, transparent 44%),
-    //       radial-gradient(circle at 95% -20%, #11649b, transparent 50%),
-    //       radial-gradient(circle at 20% 90%, #34266e, transparent 50%)
-    //   `,
-    //   xs: `
-    //       radial-gradient(circle at 10% -100%, #5f1869, transparent 44%),
-    //       radial-gradient(circle at 110% -30%, #11649b, transparent 70%),
-    //       radial-gradient(circle at 20% 90%, #34266e, transparent 50%)
-    //   `,
-    //   },
-    padding: '2rem',
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  minHeight: { lg: '100vh', xs: '90vh' },
+  background: {
+    xl: `
+      radial-gradient(circle at 20% -10%, #34266e, transparent 44%),
+      radial-gradient(circle at 80% 50%, #5f1869, transparent 29%),
+radial-gradient(circle at 25% 80%, #3b2369, transparent 44%)
+    `,
+    lg: `
+      radial-gradient(circle at 5% -60%, #34266e, transparent 44%),
+      radial-gradient(circle at 80% 50%, #5f1869, transparent 29%),
+radial-gradient(circle at 25% 80%, #3b2369, transparent 44%)
+    `,
+    xs: `
+      radial-gradient(circle at 33% 7%, #34266e, transparent 44%),
+      radial-gradient(circle at 20% 90%, #5f1869, transparent 50%),
+       radial-gradient(circle at 10% 50%, #3b2369, transparent 44%)
+    `,
+  },
+  padding: '2rem',
 }
-  
+
