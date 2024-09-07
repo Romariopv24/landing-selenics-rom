73bfadc7395819d41b859675f2de81625708be54
Romario Parra <rparra@devtop.io>
2024-09-07 16:37:37 -0400

    princing done

--- src/app/components/makeitHappen/page.tsx
+++ src/app/components/makeitHappen/page.tsx
@@ -19,12 +19,12 @@ const mainContainer = {
     xl: `
       radial-gradient(circle at 20% -10%, #34266e, transparent 44%),
       radial-gradient(circle at 80% 50%, #5f1869, transparent 29%),
-radial-gradient(circle at 25% 80%, #3b2369, transparent 44%)
+radial-gradient(circle at 25% 95%, #3b2369, transparent 50%)
     `,
     lg: `
       radial-gradient(circle at 5% -60%, #34266e, transparent 44%),
       radial-gradient(circle at 80% 50%, #5f1869, transparent 29%),
-radial-gradient(circle at 25% 80%, #3b2369, transparent 44%)
+radial-gradient(circle at 25% 95%, #3b2369, transparent 50%)
     `,
     xs: `
       radial-gradient(circle at 33% 7%, #34266e, transparent 44%),
--- src/app/components/pricing/page.tsx
+++ src/app/components/pricing/page.tsx
@@ -16,22 +16,23 @@ const mainContainer = {
   alignItems: 'center',
   flexDirection: 'column',
   minHeight: { lg: '100vh', xs: '90vh' },
-  // background: {
-  //   xl: `
-  //       radial-gradient(circle at 5% -60%, #5f1869, transparent 44%),
-  //       radial-gradient(circle at 95% -18%, #11649b, transparent 50%),
-  //       radial-gradient(circle at 20% 90%, #34266e, transparent 50%)
-  //   `,
-  //   lg: `
-  //       radial-gradient(circle at 20% -60%, #5f1869, transparent 44%),
-  //       radial-gradient(circle at 95% -20%, #11649b, transparent 50%),
-  //       radial-gradient(circle at 20% 90%, #34266e, transparent 50%)
-  //   `,
-  //   xs: `
-  //       radial-gradient(circle at 10% -100%, #5f1869, transparent 44%),
-  //       radial-gradient(circle at 110% -30%, #11649b, transparent 70%),
-  //       radial-gradient(circle at 20% 90%, #34266e, transparent 50%)
-  //   `,
-  //   },
+  background: {
+    xl: `
+           radial-gradient(circle at 25% 7%, #3b2369, transparent 50%),
+        radial-gradient(circle at 80% 50%, #4f359a, transparent 29%),
+        radial-gradient(circle at 25% 110%, #11649b, transparent 50%),
+    `,
+    lg: `
+        radial-gradient(circle at 25% 7%, #3b2369, transparent 50%),
+        radial-gradient(circle at 80% 50%, #4f359a, transparent 29%),
+        radial-gradient(circle at 25% 110%, #11649b, transparent 50%)
+    `,
+    xs: `
+        radial-gradient(circle at 10% -100%, #3b2369, transparent 44%),
+        radial-gradient(circle at 170% 6%, #4f359a, transparent 70%),
+        radial-gradient(circle at 20% 90%, #11649b, transparent 50%)
+    `,
+    },
   padding: '2rem',
 }
+
